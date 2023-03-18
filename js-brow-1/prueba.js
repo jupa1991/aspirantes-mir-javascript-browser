7503165b106b6cd20292b000bb3c40f3f4c17dc3
Juan Pablo Adarve <juanpadarve@hotmail.com>
2023-03-17 20:13:00 -0500

    elimine archivo de prueba

--- js-brow-1/prueba.js
+++
@@ -1,9 +0,0 @@
-
-
-function palindro(texto) {
-    return texto.split('').reverse().join('') == texto
-}
-
-console.log(palindro('o t t o'));
-console.log(palindro('revolver'));
-console.log(palindro('amadaladama'));
